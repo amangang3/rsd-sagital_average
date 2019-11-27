33c7160e7f736bb1dcd82574470e0445e9450456
amangang3 <aman.ganglani.16@ucl.ac.uk>
2019-11-27 09:28:17 +0000

    turned the algorithm into an installable package

---
+++ check_each_commit.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import sagital_brain
+from numpy import genfromtxt
+import subprocess
+
+data_input = np.zeros((20,20))
+data_input[-1,:] = 1
+np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',') #overwrite the brain_sample file 
+
+#run the file 
+subprocess.run(["python", "sagital_brain.py"])
+
+average_data = genfromtxt('brain_average.csv', delimiter=',') #get the data 
+
+assert sum(average_data) == 1.0
